29f8b12a5cfb3f1067812b6a3c05daf3eff25d70
felix <hunterxhunter123@hotmail.com>
2022-05-21 16:37:56 -0500

    primera version

---
+++ Main.js
@@ -0,0 +1,16 @@
+//obtengo el id con el html
+const div = $('#root');
+
+console.log(div);
+
+const h1 = '<h1 id="head">Armortización Sistema Francés</h1>';
+const p = ''
+
+//obtiene el elemento
+// #'root >'
+div.append(h1);
+
+
+
+
+
